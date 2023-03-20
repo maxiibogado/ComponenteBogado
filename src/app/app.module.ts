68e92e29117bca1baddb827fb53002147b1ecdc5
Maxi <mbogado@xubio.com>
2023-03-20 00:44:07 -0300

    Creamos el esqueleto del diseño de los 3 componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ProductoComponent } from './producto/producto.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ProductoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
